6a3de536ce0c3f9437705c8150feb21370e62799
choiyeonseok <smr603@snu.ac.kr>
2019-12-20 07:03:21 +0900

    Add inputMovieId verification

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -6,18 +6,21 @@ import java.util.List;
 import java.util.Scanner;
 
 public class InputView {
-    private static final Scanner scanner = new Scanner(System.in);
+    private static Scanner scanner;
 
     public static int inputMovieId(List<Movie> movies) {
+        scanner = new Scanner(System.in);
         System.out.println("## 예약할 영화를 선택하세요.");
         try {
             int movieId = scanner.nextInt();
             verifyMovieId(movieId, movies);
             return movieId;
         } catch (IllegalArgumentException e){
-            System.out.println("유효한 영화 ID값을 입력해 주세요");
-            return inputMovieId(movies);
+            System.out.println("유효한 영화 ID 값을 입력해 주세요");
+        } catch (Exception e) {
+            System.out.println("숫자를 입력해 주세요");
         }
+        return inputMovieId(movies);
     }
 
     private static void verifyMovieId(int id, List<Movie> movies) {
